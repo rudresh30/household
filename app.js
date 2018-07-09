141fa1fc943b6d0f1901ae1b29df00ac5bbbb81a
rudresh pandya <rudresh.pandya@gmail.com>
2018-07-09 15:55:22 +0530

    openshift commit

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ app.use(helmet());
 const staticpath = path.join(__dirname, '/');
 app.use(express.static(staticpath));
 
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.get('/', function (req, res) {
+    res.render('index.html');
+})
+
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+const ipaddr = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 app.listen(server_port, ipaddr, function () {
-    console.log(`listening on port ${server_port}`);
+    console.log(`listening on port ${server_port} and ip ${ipaddr}`);
 });
