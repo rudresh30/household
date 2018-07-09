f802e484bd2a92d7b766e0da5ac6e48b09fcb8c0
rudresh pandya <rudresh.pandya@gmail.com>
2018-07-09 15:26:30 +0530

    openshift fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const staticpath = path.join(__dirname, '/');
 app.use(express.static(staticpath));
 
 const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const ipaddr = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.listen(server_port, ipaddr, function () {
     console.log(`listening on port ${server_port}`);
 });
